a9cd2bd7e6e04956ea5ce9bc19192744f1ae7586
DANIELA MUNOZ <danielamun.832@gmail.com>
2022-11-08 16:54:09 -0600

    final-sliders

--- js/site.js
+++ js/site.js
@@ -1,4 +1,4 @@
-/*js for slider 1*/
+/*slider 1- temperature*/
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
 output.innerHTML = slider.value;
@@ -7,7 +7,7 @@ slider.oninput = function() {
   output.innerHTML = this.value;
 }
 
-/*js for slider 2*/
+/*slider 2- light cycle*/
 var slider2 = document.getElementById("myRange2");
 var output2 = document.getElementById("demo2");
 output2.innerHTML = slider2.value;
@@ -16,11 +16,38 @@ slider2.oninput = function() {
   output2.innerHTML = this.value;
 }
 
-/*js for slider 3*/
+/*slider 3- pH level*/
 var slider3 = document.getElementById("myRange3");
 var output3 = document.getElementById("demo3");
 output3.innerHTML = slider3.value;
 
 slider3.oninput = function() {
   output3.innerHTML = this.value;
+}
+
+/*slider 4- base */
+var slider4 = document.getElementById("myRange4");
+var output4 = document.getElementById("demo4");
+output4.innerHTML = slider4.value;
+
+slider4.oninput = function() {
+  output4.innerHTML = this.value;
+}
+
+/*slider 5- acid */
+var slider5 = document.getElementById("myRange5");
+var output5 = document.getElementById("demo5");
+output5.innerHTML = slider5.value;
+
+slider5.oninput = function() {
+  output5.innerHTML = this.value;
+}
+
+/*slider 6- fertilizer */
+var slider6 = document.getElementById("myRange6");
+var output6 = document.getElementById("demo6");
+output6.innerHTML = slider6.value;
+
+slider6.oninput = function() {
+  output6.innerHTML = this.value;
 }
